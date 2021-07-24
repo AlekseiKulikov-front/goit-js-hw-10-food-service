031441e3e1e1795e0be6587fa219a30ce8f6776b
Aleksei Kulikov <hanibal320@gmail.com>
2021-07-24 19:19:02 +0300

    Работа, правки....

--- src/index.js
+++ src/index.js
@@ -1,69 +1,72 @@
-//import './sass/main.scss';
+import cards from './templates/cards.hbs';
 
-import foodCardsTmp from './templates/cards.hbs';
-console.log(foodCardsTmp);
 
 import menu from './menu.json';
-console.log(menu);
+
+
+
+
+const menuList = document.querySelector('.js-menu');
+
+
+const markupCard = createMenu(menu);
+//console.log(markupCard);
+
+menuList.insertAdjacentHTML('afterbegin', markupCard);
+
+function createMenu(menu) {
+    return cards(menu);
+};
+
 
 const Theme = {
-  LIGHT: 'light-theme',
-  DARK: 'dark-theme',
-};
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
 
-// Создаем разметку по шаблону
-const menuList = document.querySelector('.js-menu');
-//console.log(menuList);
 
-const cardMarkup = createMenuList(menu);
-//console.log(cardMarkup);
 
-menuList.insertAdjacentHTML('afterbegin', cardMarkup);
+const body = document.querySelector('body');
+//console.log(body);
+body.classList.add(Theme.LIGHT);
 
-function createMenuList(menu) {
-    return foodCardsTmp(menu);
-};
 
-//По умолчанию тема светлая.
-const getBody = document.querySelector('body');
-console.log(getBody);
-getBody.classList.add(Theme.LIGHT);
+const checkbox = document.querySelector('.theme-switch__toggle');
+//console.log(checkbox);
 
-// Реализуем мозможность выбора темы
-const selectCheckbox = document.querySelector('.theme-switch__toggle');
-console.log(selectCheckbox);
 
-//При изменении темы, необходимо добавлять на элемент body класс light-theme или dark-theme
-selectCheckbox.addEventListener('change', onSelectThemes);
+checkbox.addEventListener('change', onSelectThemes);
 
-//Выбранная тема должна сохраняться между перезагрузками страницы. Для хранения темы используй localStorage.
+
 function onSelectThemes(event) {
-    if (selectCheckbox.checked) {
-        getBody.classList.remove(Theme.LIGHT);
-        getBody.classList.add(Theme.DARK);
-        const darkTheme = getBody.getAttribute('class');
-        console.log(darkTheme);
+    if (checkbox.checked) {
+        body.classList.remove(Theme.LIGHT);
+        body.classList.add(Theme.DARK);
+        const darkTheme = body.getAttribute('class');
+        //console.log(darkTheme);
         
         localStorage.setItem('theme', darkTheme);
-        console.log(localStorage.getItem('theme'));
+        //console.log(localStorage.getItem('theme'));
     }
     else {
-        getBody.classList.remove(Theme.DARK);
-        getBody.classList.add(Theme.LIGHT);
-        const lightTheme = getBody.getAttribute('class');
-        console.log(lightTheme);
+        body.classList.remove(Theme.DARK);
+        body.classList.add(Theme.LIGHT);
+        const lightTheme = body.getAttribute('class');
+        //console.log(lightTheme);
+        
         localStorage.setItem('theme', lightTheme);
     }
 
 }
 
-//Если при загрузке страницы тема тёмная, не забудь поставить свойство checked у чекбокса #theme-switch-toggle в true, чтобы ползунок сдвинулся в правильное положение.
-const getSwitchЬarker = document.querySelector('.theme-switch__marker');
-console.log(getSwitchЬarker);
+
+const switchMarker = document.querySelector('.theme-switch__marker');
+//console.log(switchMarker);
 
 if (localStorage.getItem('theme') === 'dark-theme') {
-    selectCheckbox.setAttribute('checked', 'true');
-    getBody.classList.remove(Theme.LIGHT);
-    getBody.classList.add(Theme.DARK);
-    getSwitchЬarker.style.transition = 'none';
+    checkbox.setAttribute('checked', 'true');
+    body.classList.remove(Theme.LIGHT);
+    body.classList.add(Theme.DARK);
+
+    switchMarker.style.transition = 'none';
 }
